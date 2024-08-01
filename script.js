d087f2c8928628b4869b1285df1012d25f02d59e
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2024-08-01 18:45:50 +0000

    final (?)

---
+++ script.js
@@ -0,0 +1,7 @@
+let formulario=document.querySelector(".formulario");
+function aparecerFormulario(){
+    formulario.style.left="50%";
+}
+function desaparecerFormulario(){
+    formulario.style.left="-50%";
+}
